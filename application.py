30ac907dac5167b1085ab100b0f36148d280263e
ddaarrrryyll <51407026+ddaarrrryyll@users.noreply.github.com>
2020-02-24 13:22:45 +0800

    Update application.py

--- application.py
+++ application.py
@@ -158,7 +158,7 @@ def catlink():
     # UPDATE wholesome.db
     db.execute("INSERT INTO cats (cats) VALUES(:catlink)",
                 catlink=request.form.get("catlink"))
-    db.commit()
+    conn.commit()
 
     flash("Submitted!")
     return redirect("/cats")
